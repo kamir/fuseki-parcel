b806061a2c1a4acc505d3a502d458427aa39c700
Mirko <mirko.kaempf@gmail.com>
2016-01-08 13:04:09 +0100

    Prepare the parcel builder scripts ...

---
+++ csd-src/scripts/control.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+export FUSEKI_HOME=../main/apache-jena-fuseki-2.3.0/
+
+#
+# Here we use a CLI parameter to define a location of the modelfile and the port.
+#
+
+MODEL_FILE=$2
+WEBSERVER_PORT=$3
+
+######################################
+#
+#  Etosha Triple Collector Service
+#
+
+case $CMD in
+  (start)
+    clear
+    echo "      FUSEKI_HOME: $FUSEKI_HOME"
+    echo "             PORT: $WEBSERVER_PORT"
+    echo "       MODEL_FILE: $MODEL_FILE"
+    echo " PARTITION_FOLDER: $PART_FOLDER"
+
+    echo ">>> Starting the Fuseki-Server on port [$WEBSERVER_PORT] (default: 3030)"
+
+    exec ../main/apache-jena-fuseki-2.3.0/fuseki-server --file=$MODEL_FILE --update --port=$WEBSERVER_PORT /ETCS &
+
+    sleep 2
+
+#    FILES=$PART_FOLDER/*
+#    for f in $FILES
+#    do
+#      echo "> LOAD GRAPH-PARTITION $f ..."
+#      # take action on each file. $f store current file name
+#
+#      ../main/apache-jena-fuseki-2.3.0/bin/s-post http://localhost:$WEBSERVER_PORT/ETCS/data default $f
+#    done
+
+    ;;
+  (list)
+    ../main/apache-jena-fuseki-2.3.0/bin/s-query --service http://localhost:$WEBSERVER_PORT/EC/query 'SELECT * {?s ?p ?o}'
+    ;;
+  (*)
+    echo "Don't understand [$CMD]"
+    ;;
+esac
